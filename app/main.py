c333ac407ce0df4d10ddf2a08eea7163227985ce
Lawrence Lawson <llawson@beyond.ai>
2020-05-12 23:32:29 -0700

    cleaned up logic and narrowed down endpoints
    Signed-off-by: Lawrence Lawson <llawson@beyond.ai>

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
-import os
 import json
 import webbrowser
-import time
 from turnip import Turnip
-from dotenv import load_dotenv  # for python-dotenv method
 from fastapi import FastAPI
 from typing import Dict, List
 from pydantic import BaseModel
 
 
 class TurnipItem(BaseModel):
-    name: str
+    turnip_id: str
     keywords: List[str] = None
     islands_visited: Dict[str, bool] = None
     price_threshold = 0
@@ -18,9 +15,25 @@ class TurnipItem(BaseModel):
 
 app = FastAPI()
 
-items = {
+headers = {
+    "authority": "api.turnip.exchange",
+    "accept": "application/json",
+    "content-type": "application/json",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+    "x-island-id": "",
+    "origin": "https://turnip.exchange",
+    "sec-fetch-site": "same-site",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": "https://turnip.exchange/islands",
+    "accept-language": "en-US,en;q=0.9",
+}
+data = '{"islander":"neither","category":"turnips"}'
+url = "https://api.turnip.exchange/islands/"
+
+turnip_kvs = {
     "foo": {
-        "name": "Foo",
+        "turnip_id": "Foo",
         "keywords": [
             "ENTRY",
             "entry",
@@ -34,110 +47,70 @@ items = {
             "TIP",
             "tip",
         ],
-        "price_threshold": 520,
         "islands_visited": {},
+        "price_threshold": 520,
     },
 }
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.post("/items/")
+@app.post("/turnip-items/")
 async def create_item(turnip_item: TurnipItem):
-    item_dict = turnip_item.dict()
-    return item_dict
+    turnip_item_dict = turnip_item.dict()
+    turnip_kvs[turnip_item_dict["turnip_id"]] = turnip_item_dict
+    return turnip_item_dict
 
 
 @app.get(
-    "/items/{item_id}/name",
+    "/turnip-items/{turnip_id}/name",
     response_model=TurnipItem,
-    response_model_include={"name", "keywords"},
+    response_model_include={"turnip_id"},
 )
-async def read_item_name(turnip_id: str):
-    return items[turnip_id]
+async def read_turnip_kvm_id(turnip_id: str):
+    return turnip_kvs[turnip_id]
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get(
+    "/turnip-items/{turnip_id}/visited",
+    response_model=TurnipItem,
+    response_model_include={"turnip_id", "islands_visited"},
+)
+async def read_item_islands(turnip_id: str):
+    return turnip_kvs[turnip_id]
 
 
 @app.post("/run")
-def main_driver(debug=True):
+def main_driver(turnip_id: str):
     """
     Main Logic wrapper for turnip price scraping
     """
+    if turnip_id not in turnip_kvs.keys():
+        turnip_kvs[turnip_id] = {
+            "turnip_id": turnip_id,
+            "keywords": [],
+            "islands_visited": {},
+            "price_threshold": 0,
+        }
     turnip_obj = Turnip()
-    headers = {
-        "authority": "api.turnip.exchange",
-        "accept": "application/json",
-        "content-type": "application/json",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
-        "x-island-id": "",
-        "origin": "https://turnip.exchange",
-        "sec-fetch-site": "same-site",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-dest": "empty",
-        "referer": "https://turnip.exchange/islands",
-        "accept-language": "en-US,en;q=0.9",
-    }
-    data = '{"islander":"neither","category":"turnips"}'
-    url = "https://api.turnip.exchange/islands/"
     turnip_obj.build_requests(headers=headers, data=data, url=url)
 
     # Setup filter
-    keywords = [
-        "ENTRY",
-        "entry",
-        "NMT",
-        "NMTs",
-        "nmts",
-        "GOLD",
-        "gold",
-        "MILES",
-        "miles",
-        "TIP",
-        "tip",
-    ]
-    turnip_obj.build_filter(keywords=keywords)
+    turnip_obj.build_filter(keywords=turnip_kvs[turnip_id]["keywords"])
 
     # Start main logic
-    # while True:
     response = turnip_obj.scrape_turnip_data()
-    out = json.loads(response.text)
-    print("Visited islands ", turnip_obj.islands_visited)
-    # turnip_obj.fbclient_interface(
-    #     username=os.environ.get("FB_USER"), pwd=os.environ.get("FB_PASS"), choice="Login",
-    # )
-    users = []
-    for island in out["islands"]:
+    response = json.loads(response.text)
+    visited = turnip_kvs[turnip_id]["islands_visited"]
+    print("Visited islands ", visited)
+    for island in response["islands"]:
         if (
-            island["turnipPrice"] > 550
-            and not island["turnipCode"] in turnip_obj.islands_visited.keys()
+            island["turnipPrice"] > turnip_kvs[turnip_id]["price_threshold"]
+            and not island["turnipCode"] in visited
             and not turnip_obj.keyword_processor.extract_keywords(island["description"])
         ):
             msg_url = "https://turnip.exchange/island/{}".format(island["turnipCode"])
-            print("\n", island["description"])
-            turnip_obj.islands_visited[island["turnipCode"]] = True
-            if debug:
-                webbrowser.get("chrome").open_new_tab(msg_url)
-                # users.append(turnip_obj.fbclient.uid)
-            else:
-                users_requested = ["Alex Messick", "Chris Callan"]
-                for user in users_requested:
-                    users.append((turnip_obj.fbclient.searchForUsers(user)[0]).uid)
+            # print("\n", island["description"])
+            visited[island["turnipCode"]] = True
+            webbrowser.get("chrome").open_new_tab(msg_url)
             print("ISLAND FOUND!")
-            # turnip_obj.fbclient_interface(
-            #     username=None, pwd=None, msg=msg_url, recipients=users, choice="Msg"
-            # )
-    items["bar"] = {
-        "name": "bar",
-        "keywords": keywords,
-        "islands_visited": turnip_obj.islands_visited,
-        "price_threshold": 600,
-    }
-    return items["bar"]
-    # turnip_obj.fbclient_interface(choice="Logout")
+    turnip_kvs[turnip_id]["islands_visited"] = visited
+    return turnip_kvs[turnip_id]
