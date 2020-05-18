a0493b5b50bfd1c938798cbe0be80cdb97f9c5ef
Lawrence Lawson <llawson@beyond.ai>
2020-05-17 22:50:29 -0700

    wrote test_get_villagers and test_read_villager_public
    Signed-off-by: Lawrence Lawson <llawson@beyond.ai>

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -4,6 +4,33 @@ from server import main
 
 client = TestClient(main.app)
 
+expected_get_villagers_out = {
+    "foo": {
+        "villager_id": "Foo",
+        "keywords": [
+            "ENTRY",
+            "entry",
+            "NMT",
+            "NMTs",
+            "nmts",
+            "GOLD",
+            "gold",
+            "MILES",
+            "miles",
+            "TIP",
+            "tip",
+        ],
+        "islands_visited": {},
+        "price_threshold": 520,
+    },
+    "Bar": {
+        "islands_visited": {},
+        "keywords": ["Entry"],
+        "price_threshold": 300,
+        "villager_id": "Bar",
+    },
+}
+
 
 def test_read_root():
     response = client.get("/")
@@ -27,6 +54,35 @@ def test_create_villager():
         "keywords": ["Entry"],
         "islands_visited": {},
         "price_threshold": 300,
+    }
+
+
+def test_get_villagers():
+    response = client.get("/villager/")
+    assert response.status_code == 200
+    assert response.json() == expected_get_villagers_out
+
+
+def test_read_villager_public():
+    response = client.get("/villager/foo/public")
+    assert response.status_code == 200
+    assert response.json() == {
+        "villager_id": "Foo",
+        "keywords": [
+            "ENTRY",
+            "entry",
+            "NMT",
+            "NMTs",
+            "nmts",
+            "GOLD",
+            "gold",
+            "MILES",
+            "miles",
+            "TIP",
+            "tip",
+        ],
+        "islands_visited": {},
+        "price_threshold": 520,
     }
 
 
