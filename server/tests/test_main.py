c72487c2a74ef3dc02fbc21e81114fe3faa7b251
Lawrence Lawson <llawson@beyond.ai>
2020-05-17 17:09:02 -0700

    Test create villager
    - Just simply make a post req and see if it gives back 200 and the same villager
       I posted.
       In the future we will expand and augment this to handle auth and such
    - Also, the data model is going to change.
    
    Signed-off-by: Lawrence Lawson <llawson@beyond.ai>

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -5,7 +5,31 @@ from server import main
 client = TestClient(main.app)
 
 
-def test_read_main():
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_create_villager():
+    response = client.post(
+        "/villager/",
+        json={
+            "villager_id": "Bar",
+            "keywords": ["Entry"],
+            "islands_visited": {},
+            "price_threshold": 300,
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "villager_id": "Bar",
+        "keywords": ["Entry"],
+        "islands_visited": {},
+        "price_threshold": 300,
+    }
+
+
+if __name__ == "__main__":
+    test_read_root()
+    test_create_villager()
